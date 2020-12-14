0e3974fc24e3dfd56a1be442c411516a10bcd98a
Joonas Huttunen <joonas.huttunen@myy.haaga-helia.fi>
2020-12-14 06:35:12 +0200

    kalenteri ok

--- src/components/Cal.js
+++ src/components/Cal.js
@@ -4,32 +4,53 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
 import Home from './Home';
+import axios from "axios";
 
 const localizer = momentLocalizer(moment)
 function Cal() {
-    moment.locale("en");
+    
     const [events, setEvents] = useState([]);
     const [calEvents, setCalEvents] = useState([])
 
     useEffect(() => fetchData(), []);
 
-    const fetchData = () => {
-        fetch('https://customerrest.herokuapp.com/gettrainings')
-        .then(response => response.json())
-        .then(data => setEvents(data))
-        
-       }
+    
+       const fetchData = () => {
+        axios
+          .get("https://customerrest.herokuapp.com/gettrainings")
+          .then(function(response) {
+            response.data.map(event => {
+              calEvents.push({
+                start: new Date(moment.utc(event.date).format("ll LT")),
+                end: new Date(
+                  moment
+                    .utc(event.date)
+                    .add(event.duration, "minutes")
+                    .format("ll LT")
+                ),
+                title:
+                  event.activity +
+                  " / " +
+                  event.customer.firstname +
+                  " " +
+                  event.customer.lastname
+              });
+            });
+            setEvents(calEvents);
+            console.log(calEvents);
+           });
+      };
        
 
         return (
           <div>
-            <Calendar
+            <Calendar 
               localizer={localizer}
-              events={events}
+              events={calEvents}
               startAccessor="start"
               endAccessor="end"
               style={{ height: 500 }}
-            />
+             />
           </div>
         );
       }
